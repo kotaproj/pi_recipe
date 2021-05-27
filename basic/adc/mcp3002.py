4f9502032c4fb0b18721e8df945e74f36dae35f6
kotaproj <kotaproj@gmail.com>
2021-05-27 23:38:15 +0900

    [add]mcp3002code

--- basic/adc/mcp3002.py
+++ basic/adc/mcp3002.py
@@ -1,10 +1,7 @@
 from gpiozero import MCP3002
 from gpiozero.pins.pigpio import PiGPIOFactory
-
 from time import sleep
 
-# SWのピン設定
-PIN_BTN1 = 5
 
 def main():
 
@@ -12,8 +9,7 @@ def main():
     Vref = 3.3
     adc_ch0 = MCP3002(channel=0, max_voltage=Vref, pin_factory=factory)
     while True:
-        print(adc_ch0.value * Vref, adc_ch0.value)
-        # print(adc_ch0.value * Vref)
+        print(f'value:{adc_ch0.value:.2f}, Volt:{adc_ch0.value * Vref:.2f}')
         sleep(1)
     return
 

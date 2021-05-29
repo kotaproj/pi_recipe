08bac3a160046ccc14ff09913369bb73bd75ab79
kotaproj <kotaproj@gmail.com>
2021-05-29 22:28:01 +0900

    [update]mcp3002

--- basic/adc/mcp3002.py
+++ basic/adc/mcp3002.py
@@ -4,11 +4,15 @@ from time import sleep
 
 
 def main():
-
-    factory = PiGPIOFactory()
+    # 計算用の3.3V
     Vref = 3.3
+
+    # 初期化
+    factory = PiGPIOFactory()
     adc_ch0 = MCP3002(channel=0, max_voltage=Vref, pin_factory=factory)
+
     while True:
+        # MCP3002からの出力値と電圧値を表示
         print(f'value:{adc_ch0.value:.2f}, Volt:{adc_ch0.value * Vref:.2f}')
         sleep(1)
     return
